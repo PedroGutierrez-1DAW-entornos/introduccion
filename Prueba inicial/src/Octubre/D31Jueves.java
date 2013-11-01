78844944c6a3f3919b931acb32d812f1db4bbed8
Pedro-vk <pedrogp9_6_93@hotmail.com>
2013-11-01 18:19:15 +0100

    Añadido practica del jueves 31 de octubre

---
+++ Prueba inicial/src/Octubre/D31Jueves.java
@@ -0,0 +1,40 @@
+package Octubre;
+
+public class D31Jueves {
+	/*
+	 * Algoritmo
+	 *   Optener los tres números: a, b, c (Diferentes)
+	 *   Optener el mayor
+	 *     Si a es mayor que b y c
+	 *       Escribir: mayor es a
+	 *     Sino, si b es mayor que a y c
+	 *       Escribir: mayor es b
+	 *     Sino
+	 *       Escribir: mayor es c
+	 */
+	public static void main(String[] args) {
+		// Dado tres números escribir el mayor y menor de ellos
+		int a, b, c;
+		final int BASE, RANGO;
+		
+		BASE = 1;
+		RANGO = 20;
+
+		a = aleatorio(BASE, RANGO);
+		b = aleatorio(BASE, RANGO);
+		c = aleatorio(BASE, RANGO);
+
+		if(a >= b && a >= c)
+			System.out.println("Mayor: a");
+		else if(b >= a && b >= c)
+			System.out.println("Mayor: b");
+		else
+			System.out.println("Mayor: c");
+		System.out.println("a: " + a + "\tb: " + b + "\tc: " + c);
+	}
+
+	private static int aleatorio(int base, int rango) {
+		return (int)(Math.random() * rango + base);
+	}
+
+}
