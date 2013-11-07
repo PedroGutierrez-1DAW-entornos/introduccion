cd37db69323aa65013c97335442a005b5b7ed878
Pedro-vk <pedrogp9_6_93@hotmail.com>
2013-11-08 00:27:05 +0100

    Practicas jueves 7 de noviembre

---
+++ Prueba inicial/src/Noviembre/D07Martes.java
@@ -0,0 +1,18 @@
+package Noviembre;
+
+public class D07Martes {
+	/*
+	 * Bucles
+	 */
+	public static void main(String[] args) {
+		//int i=10;
+		//while(i>0)
+		//	System.out.println(--i);
+		for(int i=0; i<20; i++){
+			if(i==11) break; // Se carga la ejecución y pasa del for
+			if(i%2>0) continue;	// Salta la ejecución de esta repetición
+			System.out.println(i);
+		}		
+	}
+
+}
