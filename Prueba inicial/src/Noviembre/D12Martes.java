81ca288cfe44aae4d780e393e342376a6f40e8a5
Pedro-vk <pedrogp9_6_93@hotmail.com>
2013-11-13 03:35:52 +0100

    Añadidos ejercicios del martes 12 de noviembre

---
+++ Prueba inicial/src/Noviembre/D12Martes.java
@@ -0,0 +1,17 @@
+package Noviembre;
+
+import Octubre.D31Jueves;
+
+public class D12Martes {
+	public static void main(String[] args) {
+		
+		int n = D31Jueves.aleatorio(0, 10), suma = 0;
+		while(n != 0){
+			System.out.print(n + " + ");
+			suma += n;
+			n = D31Jueves.aleatorio(0, 10);
+		}
+		System.out.print(n + " = " + suma);
+		
+	}
+}
